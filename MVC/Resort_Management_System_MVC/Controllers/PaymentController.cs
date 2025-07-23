292b0cc644d257afb8f5b1ad83aa75ee49c2d1e2
Riyabhimani <riyabhimani.me@gmail.com>
2025-07-23 09:00:04 +0530

    MVC

---
+++ MVC/Resort_Management_System_MVC/Controllers/PaymentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Resort_Management_System_MVC.Models;
+
+namespace Resort_Management_System_MVC.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly HttpClient client;
+
+
+        public PaymentController(IHttpClientFactory httpClientFactory)
+        {
+            client = httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("http://localhost:5159/api/Payment");
+        }
+
+        public async Task<IActionResult> PaymentList()
+        {
+            var response = await client.GetAsync("Payment");
+            var json = await response.Content.ReadAsStringAsync();
+            var list = JsonConvert.DeserializeObject<List<PaymentModel>>(json);
+            return View(list);
+        }
+        public async Task<IActionResult> PaymentDelete(int id)
+        {
+
+            try
+            {
+                await client.DeleteAsync($"Payment/{id}");
+                //TempData["SuccessMessage"] = "Payment deleted successfully.";
+                return RedirectToAction("PaymentList");
+            }
+
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred while deleting the Payment: " + ex.Message;
+                return RedirectToAction("PaymentList");
+            }
+        }
+    }
+}
---
+++ MVC/Resort_Management_System_MVC/Models/GuestDropdownModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Resort_Management_System_MVC.Models
+{
+    public class GuestDropdownModel
+    {
+
+        public int GuestId { get; set; }
+
+        public string FullName { get; set; }
+
+    }
+}
